d3e9cb5577e7e7f054de93a9e80de0726b928430
ufy <ufuomaitwa@gmail.com>
2024-06-11 23:07:18 +0100

    newsletter updated

--- assets/js/newsletter-modal.js
+++ assets/js/newsletter-modal.js
@@ -5,10 +5,38 @@ if (!customElements.get('newletter-modal')) {
 
             this.close = this.querySelector('.news-modal-close');
             this.close.addEventListener("click", this.disablePopup.bind(this));
+
+            this.loader = this.querySelector('#spinloader');
+
+            this.email = this.querySelector('#email');
+
+            this.signUp = this.querySelector('#signupButton');
+            this.signUp.addEventListener("click", this.onSubmit.bind(this))
         }
 
         disablePopup() {
             localStorage.setItem('disabledPopup', 'yes');
+            setTimeout(() => {
+                localStorage.removeItem('disabledPopup');
+            }, 90000)
+        }
+
+        onSubmit(event) {
+            event.preventDefault();
+            this.loader.classList.remove("hidden")
+            if ( this.email.value !== "") {
+                const config = fetchConfig();
+                config.body = JSON.stringify({email: this.email.value})
+                console.log(config.body)
+                fetch(`${routes.newsletter_signup}`, config)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+            }
         }
     })
 }
