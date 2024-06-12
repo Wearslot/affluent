a0e0baa5421a7fe0ec5427d21c5a544abd982d86
ufy <ufuomaitwa@gmail.com>
2024-06-13 00:46:12 +0100

    edits to newsletter

--- assets/js/newsletter-modal.js
+++ assets/js/newsletter-modal.js
@@ -4,14 +4,16 @@ if (!customElements.get('newletter-modal')) {
             super();
 
             this.close = this.querySelector('.news-modal-close');
+            console.log(this.close)
             this.close.addEventListener("click", this.disablePopup.bind(this));
 
-            this.feedback = this.querySelector('#email_feedback');
+            // this.feedback = this.querySelector('#email_feedback');
 
-            this.email = this.querySelector('#email');
+            // this.email = this.querySelector('#email');
 
-            this.signUp = this.querySelector('#signupButton');
-            this.signUp.addEventListener("click", this.onSubmit.bind(this))
+            // this.signUp = this.querySelectorAll('#signupButton').forEach(button => {
+            //     button.addEventListener("click", this.onSubmit.bind(this));
+            // });
         }
 
         disablePopup() {
@@ -21,29 +23,39 @@ if (!customElements.get('newletter-modal')) {
             }, 90000)
         }
 
-        onSubmit(event) {
-            event.preventDefault();
-            this.signUp.querySelector('.loading__spinner').classList.remove('hidden');
-            this.signUp.disabled = true
-            if ( this.email.value !== "") {
-                const config = fetchConfig();
-                config.body = JSON.stringify({email: this.email.value})
-                fetch(`${routes.newsletter_signup}`, config)
-                .then(response => response.json())
-                .then(data => {
-                    this.signUp.querySelector('.loading__spinner').classList.add('hidden');
-                    this.feedback.innerText= data.message;
-                })
-                .catch(e => {
-                    console.log(e);
-                });
-            }
-            else {
-                this.signUp.querySelector('.loading__spinner').classList.add('hidden');
-                this.feedback.innerText= "Please enter your email";
-                this.email.value = ""
-                this.signUp.disabled = false
-            }
-        }
+        // onSubmit(event) {
+        //     event.preventDefault();
+        //     this.activateLoadingState()
+
+        //     if ( this.email.value !== "") {
+
+        //         const config = fetchConfig();
+        //         config.body = JSON.stringify({email: this.email.value})
+        //         fetch(`${routes.newsletter_signup}`, config)
+        //         .then(response => response.json())
+        //         .then(data => {
+        //             this.deactivateLoadingState()
+        //             this.feedback.innerText= data.message;
+        //         })
+        //         .catch(e => {
+        //             console.log(e);
+        //         });
+        //     }
+        //     else {
+        //         this.deactivateLoadingState()
+        //         this.feedback.innerText= "Please enter your email";
+        //     }
+        // }
+
+        // activateLoadingState() {
+        //     this.signUp.querySelector('.loading__spinner').classList.remove('hidden');
+        //     this.signUp.disabled = true
+        // }
+
+        // deactivateLoadingState() {
+        //     this.signUp.querySelector('.loading__spinner').classList.add('hidden');
+        //     this.email.value = ""
+        //     this.signUp.disabled = false
+        // }
     })
 }
